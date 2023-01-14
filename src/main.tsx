86d6a1e555d9fa91e58a9a87b71a4cee348626ec
Newton Lomar <newtonlomar16@gmail.com>
2023-01-14 12:36:20 -0300

    criando as primeira rotas com BrowserRoute, Routes e Route

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./components/Home";
+import { About } from "./components/About";
 import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );

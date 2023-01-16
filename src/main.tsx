11ac16df4640a2573e69013f0bfdb2e058a7936c
Newton Lomar <newtonlomar16@gmail.com>
2023-01-16 12:01:16 -0300

    Usando o componente Link do react router dom  ao invés de <a>

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./components/Home";
 import { About } from "./components/About";
 import "./styles/global.css";
+import { Menu } from "./components/Menu";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
+      <Menu />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
